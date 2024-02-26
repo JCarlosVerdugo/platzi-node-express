b0d8d975ce01f077f3407fd4c5a3386054e7736d
Carlos Verdugo <carlosverdugo@proton.me>
2024-02-25 18:37:04 -0700

    Adding router product

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const routerApi = require('./routes');
+
+const app = express();
+const port = 3000;
+
+// app.set('port', process.env.port || 3000);
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId, productId} = req.params;
+
+  res.json({
+    categoryId,
+    productId,
+    name: 'Product 2',
+    price: 2000
+  })
+
+});
+
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay queries')
+  }
+
+});
+
+routerApi(app);
+
+
+
+// app.listen(app.get('port'), () => {
+//   console.log(`Server on port ${app.get('port')}`)
+// })
+
+app.listen(port, () => {
+  console.log(`My port ${port}`);
+})
